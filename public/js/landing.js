a61be15f7f6f048b47caf97eb0f3dbe311e41f1a
Michael Mazzone <mazzone.deca@gmail.com>
2017-09-17 04:03:20 -0400

    Creating user object

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
-	$('#yearInSchoolDropdown').change(function() {
+	var uniNameSelected = "";
+	var yearInSchool = "";
+
+	$('#autocomplete-input-uniName').change(function() {
 		console.log($(this).val());
+		uniNameSelected = $(this).val();
 	});
 
-	$('#autocomplete-input-uniName').uniInput(function() {
+	$('#yearInSchoolDropdown').change(function() {
 		console.log($(this).val());
+		yearInSchool = $(this).val();
 	});
+
+	var languagesKnown = [];
+	$('#autocomplete-input-progLang').change(function() {
+		console.log($(this).val());
+		//append each value to languagesKnown
+	});
+
+	var userObj = {
+		"userEmail" : "none@gmail.com",
+		"userSchool" : uniNameSelected,
+		"languagesKnown" : "PLACEHOLDER", //REPLACE WITH ARRAY LANGUAGESKNOWN
+		"yearInSchool": yearInSchool,
+		"timeCommitment" : "PLACEHOLDER" //REPLACE WITH VALUE CHOSEN
+	}
 });
--- public/js/main.js
+++ public/js/main.js
@@ -14,13 +14,11 @@ $(document).ready(function() {
 
 function getUniversityName() {
   var universityName = document.getElementById("autocomplete-input-uniName").value;
-  console.log("Uni name: " + universityName);
   return universityName;
 }
 
 function getProgrammingLanguage() {
   var programmingLanguage = document.getElementById("autocomplete-input-progLang").value;
-  console.log("Programming language: " + programmingLanguage);
   return programmingLanguage;
 }
 

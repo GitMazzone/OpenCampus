cf46904918f83a357b5b3bef3261b9f25b93a2c7
Varun Ramachandran <varun.ramachandran@newellco.com>
2017-09-16 22:20:10 -0400

    heroku update

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ app.get("/confirmed", function(req, res) {
             }, 
             qs:qs, 
             json:true}, function (e, r, user) {
-              user = JSON.parse(user);
               console.log(user[0].email);
               req.session.user = user[0].email;
           })

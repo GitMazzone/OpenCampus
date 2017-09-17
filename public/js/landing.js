80b6f362aa11997fcef53d5ebba0f63891035b27
Benrflanders <benrflanders@yahoo.com>
2017-09-17 03:09:51 -0400

    University name prints to console

--- public/js/landing.js
+++ public/js/landing.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$('#yearInSchoolDropdown').change(function() {
 		console.log($(this).val());
 	});
+
+	$('#autocomplete-input-uniName').uniInput(function()){
+		console.log($(this).val());
+	});
 });
+
